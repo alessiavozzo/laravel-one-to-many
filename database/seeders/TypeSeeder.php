d2b6c30f1e6f725c8aa9d070d3506b1fafaf2a89
alessiavozzo <a.vozzo95@gmail.com>
2024-05-23 13:11:17 +0200

    add type migration and type seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['Frontend', 'Backend', 'Fullstack', 'GraphicDesign', 'MobileApp', 'Game'];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name, '-');
+            $newType->save();
+        }
     }
 }
