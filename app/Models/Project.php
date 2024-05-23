5547e96e95183d80be52644c2e1ae237dc22149c
alessiavozzo <a.vozzo95@gmail.com>
2024-05-23 13:46:19 +0200

    add one to many relationship: a project belongs to one type, but one type can be associated with many projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'slug', 'project_image', /* 'tools' */'preview', 'project_link', 'github_link', 'creation_date', 'description'];
+    protected $fillable = ['title', 'slug', 'project_image', /* 'tools' */'preview', 'project_link', 'github_link', 'creation_date', 'description', 'type_id'];
+
+    /**
+     * Get the user that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
